18d7adda60c1d2768547b5d094834fcf4a9d5ff6
MiteshZimbra <mitesh.savani@synacor.com>
2020-06-29 23:21:48 +0530

    transition: Added right click, left click Page navigation

--- src/routes/FunFacts.js
+++ src/routes/FunFacts.js
@@ -9,7 +9,64 @@ class FunFacts extends React.Component {
         this.state = {
             counter: 1
         }
+        this.rightBlockRef = React.createRef();
+        this.leftBlockRef = React.createRef();
+        this.mainBlockRef = React.createRef();
+        this.footerRef = React.createRef();
     }
+
+    rightBlockHandler = () => {
+        const block1 = this.leftBlockRef.current;
+        block1.classList.toggle("Block_1_0_transition");
+
+        const block2 = this.mainBlockRef.current;
+        block2.classList.toggle("Block_2_1_transition");
+
+        const footerBlock = this.footerRef.current;
+        footerBlock.classList.toggle("footerBlock-transition");
+
+        const block3 = this.rightBlockRef.current;
+        block3.classList.toggle("Block_3_2_transition");
+
+        setTimeout(() => {
+            block1.classList.toggle("Block_1_0_transition");
+
+            block2.classList.toggle("Block_2_1_transition");
+
+            block3.classList.toggle("Block_3_2_transition");
+
+            footerBlock.classList.toggle("footerBlock-transition");
+
+        }, 1500);
+        
+    }
+
+    leftBlockHandler = () => {
+        const block1 = this.leftBlockRef.current;
+        block1.classList.toggle("Block_1_2_transition");
+
+        const block2 = this.mainBlockRef.current;
+        block2.classList.toggle("Block_2_3_transition");
+
+        const footerBlock = this.footerRef.current;
+        footerBlock.classList.toggle("footerBlock-transition");
+
+        const block3 = this.rightBlockRef.current;
+        block3.classList.toggle("Block_3_0_transition");
+
+
+        setTimeout(() => {
+            block1.classList.toggle("Block_1_2_transition");
+
+            block2.classList.toggle("Block_2_3_transition");
+
+            block3.classList.toggle("Block_3_0_transition");
+
+            footerBlock.classList.toggle("footerBlock-transition");
+
+        }, 2600);
+    }
+
 
     render() {
         return (
@@ -20,8 +77,8 @@ class FunFacts extends React.Component {
                             <div>Prev</div>
                         </div> */}
 
-                        <div className="parent-block">
-                            <div className="block main-block block-1">
+                        <div className="parent-block" >
+                            <div className="block main-block block-1" ref={this.leftBlockRef} onClick={this.leftBlockHandler}>
                                 <div className="block-title">
                                 <h4>Title of 1</h4>
 
@@ -34,7 +91,7 @@ class FunFacts extends React.Component {
                                 </div>
                             </div>
 
-                            <div className="block main-block block-2">
+                            <div className="block main-block block-2" ref={this.mainBlockRef}>
                                 <div className="block-title">
                                 <h4>Title of 2</h4>
 
@@ -42,12 +99,12 @@ class FunFacts extends React.Component {
                                 <div className="blocks-description">
                                     <span>Description of 1</span>
                                 </div>
-                                <div className="block-footer">
+                                <div className="block-footer" ref={this.footerRef}>
                                     <button className="more"> More </button>
                                 </div>
                             </div>
 
-                            <div className="block main-block block-3">
+                            <div className="block main-block block-3" ref={this.rightBlockRef} onClick={this.rightBlockHandler}>
                                 <div className="block-title">
                                     <h4>Title of 3</h4>
                                 </div>
