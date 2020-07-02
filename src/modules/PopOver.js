642787d929a6332a392ffea67487c1d2b3779d5d
jigarkakdiya38 <jigarkakdiya38@gmail.com>
2020-07-02 22:58:02 +0530

    added more facts on the same one

--- src/modules/PopOver.js
+++ src/modules/PopOver.js
@@ -52,6 +52,50 @@ const PopOver = ({ popRef, closePopupHandler }) => {
 
                         </div>
                     </div>
+                    <div className="row mar-bottom-10">
+                        <div className="col-lg-1">
+                            <span className="bullet-point">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div className="col-lg-11 pad-left-0">
+                            <label className="font-size-21 font-family-cur">Big pythons and boa snakes have traces of back legs left over from millions of years of evolution when snakes started out as land lizards. As if we didn’t have enough to worry about.</label>
+
+                        </div>
+                    </div>
+                    <div className="row mar-bottom-10">
+                        <div className="col-lg-1">
+                            <span className="bullet-point">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div className="col-lg-11 pad-left-0">
+                            <label className="font-size-21 font-family-cur">It can take a snake up to five days to digest its last meal. With bigger snakes like an anaconda, that digestion period is weeks rather than days, the same amount of time after an Italian Thanksgiving dinner. Trust me.</label>
+
+                        </div>
+                    </div>
+                    <div className="row mar-bottom-10">
+                        <div className="col-lg-1">
+                            <span className="bullet-point">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div className="col-lg-11 pad-left-0">
+                            <label className="font-size-21 font-family-cur">The Mongoose is one of those rare animals that is immune to a snake’s venom. Go, Mongoose!</label>
+
+                        </div>
+                    </div>
+                    <div className="row mar-bottom-10">
+                        <div className="col-lg-1">
+                            <span className="bullet-point">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </span>
+                        </div>
+                        <div className="col-lg-11 pad-left-0">
+                            <label className="font-size-21 font-family-cur">Record holder for biggest variety of snake goes to the anaconda. It can top out at 595 pounds (270kg) and stretch out to 30 feet (9 meters). How big is that? An anaconda has been known to snack on full-grown jaguars… the cat, not the car.</label>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
